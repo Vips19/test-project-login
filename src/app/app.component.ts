811600226ca7f50c84056e9ee93f09c7a22da5f4
Vips19 <vibhashpratik19@gmail.com>
2019-05-20 00:43:39 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpServiceService, Post } from './http-service.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-project-test';
+export class AppComponent implements OnInit {
+  inputid:number;
+  post:Post[];
+  constructor(private postService:HttpServiceService){}
+
+  ngOnInit(){
+
+  }
+
+  getPostDetails(){
+     this.postService.getPost(this.inputid).subscribe(
+      data=>{
+        this.post=data;
+        console.log(this.post);
+      }
+
+      
+    );
+  }
 }
